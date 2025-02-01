34948000fddcc8c0e3c0a730417336972ce479e9
W1SL4 <konzaw150798@gmail.com>
2025-02-01 13:50:40 +0100

    Dodano funkcje sprawdzajaca

--- projekt.cpp
+++ projekt.cpp
@@ -2,6 +2,19 @@
 
 namespace std;
 
+bool czy_pierwsza(int n) {
+    	if (n < 2) return false;   
+	
+	for (int i = 2; i * i <= n; i++) {
+		
+		if (n % i == 0) return false;
+    		}   
+	
+	return true;
+	
+	}
+
+
 int main()
 
 {
@@ -9,7 +22,7 @@ int main()
 
 	cin >> liczba;
 
-	cout << liczba << endl;
+	cout << (czy_pierwsza(liczba) ? "jest liczba pierwsza" : "nie jest liczba pierwsza" << endl;
 
 	return 0;
 
